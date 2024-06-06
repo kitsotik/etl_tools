2e4baef809fff205cf70d63fd3297f25918f1356
kitsotik <gonchibud@gmail.com>
2024-06-06 12:51:32 -0300

    Actualizacion realizada el $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')

--- importar_categorias/categorias_import.py
+++ importar_categorias/categorias_import.py
@@ -1,5 +1,5 @@
 import xmlrpc.client
-import openpyxl
+import csv
 
 class OdooConnector:
     def __init__(self, dbname, username, password, url):
@@ -12,35 +12,36 @@ class OdooConnector:
         self.models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
 class CategoryImporter:
-    def __init__(self, odoo, excel_file):
+    def __init__(self, odoo, csv_file):
         self.odoo = odoo
-        self.excel_file = excel_file
+        self.csv_file = csv_file
 
     def import_categories(self, model):
-        workbook = openpyxl.load_workbook(self.excel_file)
-        sheet = workbook.active
-        for row in sheet.iter_rows(min_row=2, values_only=True):
-            name, parent_name = row
-            if not name:
-                continue
-            parent_id = False
-            if parent_name:
-                parent_id = self.find_category_by_name(model, parent_name)
-            vals = {'name': name}
-            if parent_id:
-                vals['parent_id'] = parent_id
-            self.odoo.models.execute_kw(self.odoo.dbname, self.odoo.uid, self.odoo.password,model, "create", [vals])
+        with open(self.csv_file, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader)  # Skip header row
+            for row in reader:
+                name, parent_name = row
+                if not name:
+                    continue
+                parent_id = False
+                if parent_name:
+                    parent_id = self.find_category_by_name(model, parent_name)
+                vals = {'name': name}
+                if parent_id:
+                    vals['parent_id'] = parent_id
+                self.odoo.models.execute_kw(self.odoo.dbname, self.odoo.uid, self.odoo.password, model, "create", [vals])
 
     def find_category_by_name(self, model, name):
         domain = [('name', '=', name)]
-        category = self.odoo.models.execute_kw(self.odoo.dbname, self.odoo.uid, self.odoo.password,model, "search_read", [domain])
+        category = self.odoo.models.execute_kw(self.odoo.dbname, self.odoo.uid, self.odoo.password, model, "search_read", [domain])
         if category:
             return category[0]['id']
         return False
 
 if __name__ == "__main__":
-    odoo = OdooConnector('o16db', 'admin', 'bgt56yhn*971', 'http://localhost:8069')
-    importer = CategoryImporter(odoo, '_categorias.xlsx')
+    odoo = OdooConnector('o16db', 'admin', 'bgt56yhn*971', 'http://192.168.192.131:8069')
+    importer = CategoryImporter(odoo, '_categorias.csv')
     importer.import_categories('product.category')
     importer.import_categories('pos.category')
     importer.import_categories('product.public.category')
